0d842180dc7ee41620e33a944822c1b0d8db2af2
Lesedi <u22564812@tuks.co.za>
2025-06-24 12:04:34 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./frontend/index.html', './frontend/src/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+};
