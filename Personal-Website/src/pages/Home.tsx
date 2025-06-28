19c554d3e57526e3b4c1a0c9683d3855465ca8ef
Lesedi <u22564812@tuks.co.za>
2025-06-28 13:49:32 +0200

    Home page

---
+++ Personal-Website/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-950 text-white">
+      <Navbar />
+      {/* Page content goes here */}
+      <div className="p-6">
+        
+      </div>
+    </div>
+  );
+}
