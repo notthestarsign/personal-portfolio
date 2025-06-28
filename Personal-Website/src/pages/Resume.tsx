aeb40744236bb6f574f1df6d07d6f5f56d56cdd2
Lesedi <u22564812@tuks.co.za>
2025-06-28 13:47:14 +0200

    Projects and Resume

---
+++ Personal-Website/src/pages/Resume.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/Navbar";
+
+export default function Projects() {
+  return (
+    <div className="bg-gray-950 text-white">
+      <Navbar />
+      {/* Page content goes here */}
+      <div className="p-6">
+        
+      </div>
+    </div>
+  );
+}
