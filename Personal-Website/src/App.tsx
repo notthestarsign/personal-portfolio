a487c0a919ad8934b3d5fe8aa5e61d8a5cfb61bb
Lesedi <u22564812@tuks.co.za>
2025-06-28 12:53:51 +0200

    Route for home page

--- Personal-Website/src/App.tsx
+++ Personal-Website/src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Routes, Route } from "react-router-dom"
-import HomePage from '@/pages/HomePage';
+import Landing from '@/pages/Landing';
+import Home from '@/pages/Home';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +9,8 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
     </>
   )
