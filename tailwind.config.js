c25a4f7d00ba666ffbfd87beccbe1c20b040e2c7
Lesedi <u22564812@tuks.co.za>
2025-12-10 20:44:13 +0200

    fix tailwind config file

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./frontend/index.html', './frontend/src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
