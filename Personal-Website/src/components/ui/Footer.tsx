29c08b4f804bcd74709a8222d54c1b3bebe2f412
Lesedi <u22564812@tuks.co.za>
2025-12-10 21:14:03 +0200

    Fix let's connect buttons

--- Personal-Website/src/components/ui/Footer.tsx
+++ Personal-Website/src/components/ui/Footer.tsx
@@ -6,15 +6,15 @@ export default function Footer() {
     <footer className="mt-20 py-6 text-center border-t border-yellow-400">
       <p className="text-gray-400 mb-4">Let's connect:</p>
               <div className="flex justify-center gap-6 text-yellow-400 text-2xl">
-                <Link to="https://github.com/notthestarsign" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/notthestarsign" target="_blank" rel="noopener noreferrer">
                   <FaGithub />
-                </Link>
-                <Link to="https://linkedin.com/in/lesedimanoto" target="_blank" rel="noopener noreferrer">
+                </a>
+                <a href="https://linkedin.com/in/lesedimanoto" target="_blank" rel="noopener noreferrer">
                   <FaLinkedin />
-                </Link>
-                <Link to="mailto:lesedielliotmanoto06@gmail.com" target="_blank" rel="noopener noreferrer">
+                </a>
+                <a href="mailto:lesedielliotmanoto06@gmail.com" target="_blank" rel="noopener noreferrer">
                   <FaEnvelope />
-                </Link>
+                </a>
               </div>
               <p className="text-gray-600 text-sm mt-4">© {new Date().getFullYear()} Lesedi Manoto</p>
     </footer>
